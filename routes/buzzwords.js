4aec4cdb8db930aadd1ba2b6eb428a4ce5715992
Alexander Davis <akonidavis@gmail.com>
2017-04-18 19:22:22 -1000

    can post

--- routes/buzzwords.js
+++ routes/buzzwords.js
@@ -1,15 +1,19 @@
 /*jshint esversion:6*/
 const express = require('express');
-
 const router = express.Router();
+
+
+let buzziestWords = [];
 
 router.route('/')
   .get(function(req, res) {
-    res.send(`Got Buzzword by id ${req.params.id}`);
+    res.send(`Got Buzzword `);
   })
   .post(function(req, res) {
   	//console.log(req.body);
-    res.send(`Buzzword added: ${req.params.id}`);
+    buzziestWords.push(req.body);
+    console.log(buzziestWords);
+    res.send(`Buzzword added`);
   })
   .put(function(req, res) {
     res.send('Update the movie');
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const buzzwordsRoutes = require('./routes/buzzwords');
 const PORT = 3000;
 
+let buzziestWords = [];
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -15,8 +17,6 @@ app.use('/buzzwords', buzzwordsRoutes);
 app.get('/', function(req,res) {
 	res.send('Hello World!');
 });
-
-
 
 const server = app.listen(PORT, () => {
 	console.log(`server listening on port ${PORT}`);
